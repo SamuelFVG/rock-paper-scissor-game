6d4335de88dfcab913078039a9ac1c757cef47fd
SamuelFVG <samuelfelipeif@gmail.com>
2022-01-06 20:12:02 -0300

    Created a function called getWinner() to get the winned based on user and computer input

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function getComputerChoice(){
+function getWinner(user, computer) {
+    let winner;
+
+    if (user == computer) {
+        winner = '';
+    }
+    else if ((user == 'r' && computer == 's') || (user == 'p' && computer == 'r') || (user == 's' && computer == 'p')){
+        winner = 'user';
+    }
+    else{
+        winner = 'computer';
+    }
+    
+    return winner;
+}
+
+function getComputerChoice() {
     options = ['r', 'p', 's'];
     choice = options[Math.floor(Math.random()*3)];
     return choice;
 }
-function playGameRound(){
+
+function playGameRound() {
     const userChoice = prompt('Rock, paper or scissor?').toLowerCase().charAt(0); // first character of string ('lowercased')
     const computerChoice = getComputerChoice();
-    console.log(userChoice+" "+computerChoice);
+    
+    return getWinner(userChoice, computerChoice);
 }
